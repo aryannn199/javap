9b1730be6203e39aa95a31a4357297be4ae84a97
aryannn199 <164353303+aryannn199@users.noreply.github.com>
2025-01-21 08:50:36 +0530

    Add files via upload

---
+++ JavaLab/src/module3/Account.java
@@ -0,0 +1,23 @@
+package module3;
+
+public class Account {
+	
+	private int accNO;
+	private String accName;
+	
+	
+	public int getAccNO() {
+		return accNO;
+	}
+	public void setAccNO(int accNO) {
+		this.accNO = accNO;
+	}
+	public String getAccName() {
+		return accName;
+	}
+	public void setAccName(String accName) {
+		this.accName = accName;
+	}
+	                    
+	
+}
